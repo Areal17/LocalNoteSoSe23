824a4b96bbb958b787afff105087536179ad1a3b
Areal17 <post@media-engineering.com>
2023-06-25 18:45:43 +0200

    Initial Commit

---
+++ LocalNote2023/LocalNote2023App.swift
@@ -0,0 +1,17 @@
+//
+//  LocalNote2023App.swift
+//  LocalNote2023
+//
+//  Created by Ingo Wiederoder on 25.06.23.
+//
+
+import SwiftUI
+
+@main
+struct LocalNote2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
